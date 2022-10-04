f2a356e33ba9c2fdb85117a220d371df3d016083
RvrCS <75063620+RvrCS@users.noreply.github.com>
2022-10-04 11:11:08 -0700

    Resizable
    Venta de partida no resizable

--- Patolli_gui/src/ventanaTablero.java
+++ Patolli_gui/src/ventanaTablero.java
@@ -48,6 +48,7 @@ public class ventanaTablero extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
