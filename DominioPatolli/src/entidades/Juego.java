df5eadb31a3fec214cfe832fe944b57a359c5947
RvrCS <75063620+RvrCS@users.noreply.github.com>
2022-10-09 19:50:23 -0700

    UML & Clases nuevas
    Uml actualizado
    Clase juego creada junto con clase jugador creada

--- DominioPatolli/src/entidades/Juego.java
+++ DominioPatolli/src/entidades/Juego.java
@@ -1,4 +1,50 @@
 package entidades;
+
+import java.util.List;
+
 public class Juego {
-	
+    private Tablero tablero;
+    private int apuesta, fondoFijo;
+    private List<Jugador> jugadores;
+
+    public Juego(Tablero tablero, int apuesta, int fondoFijo, List<Jugador> jugadores) {
+        this.tablero = tablero;
+        this.apuesta = apuesta;
+        this.fondoFijo = fondoFijo;
+        this.jugadores = jugadores;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Tablero tablero) {
+        this.tablero = tablero;
+    }
+
+    public int getApuesta() {
+        return apuesta;
+    }
+
+    public void setApuesta(int apuesta) {
+        this.apuesta = apuesta;
+    }
+
+    public int getFondoFijo() {
+        return fondoFijo;
+    }
+
+    public void setFondoFijo(int fondoFijo) {
+        this.fondoFijo = fondoFijo;
+    }
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+    
+    
 }
