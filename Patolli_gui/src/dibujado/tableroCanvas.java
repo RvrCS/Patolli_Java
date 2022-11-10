40dcf5ba0ffbd61d38bd7e68c8083c2e7f04f03c
RvrCS <75063620+RvrCS@users.noreply.github.com>
2022-11-09 20:25:59 -0700

    Backup Patolli

--- Patolli_gui/src/dibujado/tableroCanvas.java
+++ Patolli_gui/src/dibujado/tableroCanvas.java
@@ -20,29 +20,43 @@ public class tableroCanvas extends JPanel{
 	private int numCasillasAspa;
 	private int anchoPantalla;
 	private Graphics2D g2d;
-	protected  final int TAMANIOCASILLA = 25;
+	private  final int TAMANIOCASILLA = 25;
+	private dibujaCasilla dc;
 	
+	/**
+	 * Método constructor que recibe e instancia los valores  enviados
+	 * @param casillas
+	 * @param numCasillasAspa
+	 * @param anchoPantalla 
+	 */
 	
 	public tableroCanvas(LinkedList<Casilla> casillas, int numCasillasAspa, int anchoPantalla) {
 		this.casillas = casillas;
 		this.numCasillasAspa = numCasillasAspa;
 		this.anchoPantalla=anchoPantalla/6;
-//		this.ancho = casillas.size() * 50 + 50 * 3;
-//		this.alto=ancho;
+		this.dc = new dibujaCasilla();
 		inicializar();
 	}
 	
+	/**
+	 * Metodo que pone en blanco el fondo de la ventana
+	 */
 	private void inicializar() {
 		this.setBackground(new Color(0, 0, 0, 0));
 	}
 	
+	
+	/**
+	 * Metodo que dibuja todas las casillas del tablero, estas se dibujan mediante la lista Casillas
+	 * @param g 
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		this.g2d = (Graphics2D) g;
 		for (Casilla casilla : casillas) {
 			if (casilla.getTipoCasilla().equalsIgnoreCase("centro")) {
-				this.dibujarCentro(g2d, casilla, numCasillasAspa);
+				this.dibujarCentro(g2d, casilla/**, numCasillasAspa**/);
 			} else if (casilla.getTipoCasilla().equalsIgnoreCase("circulo")) {
 				this.dibujarCirculo(g2d, casilla);
 			}else if (casilla.getTipoCasilla().equalsIgnoreCase("triangulo")) {
@@ -54,6 +68,12 @@ public class tableroCanvas extends JPanel{
 			}
 		}
 	}
+	
+	/**
+	 * Metodo que genera todas las casillas del juego
+	 * Aqui se calcula en donde se dibujara espesificamente cada casilla
+	 * @return 
+	 */
 	
 	public LinkedList<Casilla> generarCasillas() {
 		int tamanioCasilla = TAMANIOCASILLA;
@@ -224,133 +244,169 @@ public class tableroCanvas extends JPanel{
 		return casillas;
 	}
 	
+	/**
+	 * Metodo que establece una lista de casillas por otra recibida en el parametro
+	 * @param casillas 
+	 */
 	public void setCasillas(LinkedList<Casilla> casillas) {
 		this.casillas = casillas;
 	}
 	
-	public void dibujarCentro(Graphics2D g2d,Casilla casilla, int numCasillasAspa) {
-		Graphics g;
-		g2d.setStroke(new BasicStroke(2));
-		Rectangle rect = new Rectangle();
-		g2d.setColor(Color.CYAN);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.fill(rect);
-		g2d.draw(rect);
-		g2d.setColor(Color.BLACK);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.draw(rect);
+	/**
+	 * Metodo que dibuja exclusivamente la casilla del centro, mediante los parametros recibidos
+	 * @param g2d
+	 * @param casilla
+	 * @param numCasillasAspa 
+	 */
+	public void dibujarCentro(Graphics2D g2d,Casilla casilla/**, int numCasillasAspa**/) {
+		this.dc.dibujarCentro(g2d, casilla, numCasillasAspa, TAMANIOCASILLA);
+		
+//		Graphics g;
+//		g2d.setStroke(new BasicStroke(2));
+//		Rectangle rect = new Rectangle();
+//		g2d.setColor(Color.CYAN);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.fill(rect);
+//		g2d.draw(rect);
+//		g2d.setColor(Color.BLACK);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.draw(rect);
 		
 	}
+	
+	/**
+	 * Metodo que dibuja exclusivamente el final de las aspas de el tablero, mediante los parametros recibidos
+	 * @param g2d
+	 * @param casilla 
+	 */
 	    public void dibujarCirculo(Graphics2D g2d,Casilla casilla) {
-		Shape punta = null;
-		g2d.setStroke(new BasicStroke(2));
-		if (casilla.getLugarcirculo() != null) {
-			int x = casilla.getCoordenadaX();
-			int y = casilla.getCoordenadaY();
-			if (numCasillasAspa % 2 == 0) {
-				switch ( casilla.getLugarcirculo()) {
-					case TOP:
-						punta = new Arc2D.Double(x, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 0, 180, Arc2D.OPEN);
-						g2d.setColor(Color.GRAY);
-						g2d.fill(punta);
-						g2d.setColor(Color.BLACK);
-						g2d.drawLine(x + TAMANIOCASILLA, y, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
-						break;
-					case LEFT:
-						punta = new Arc2D.Double(x, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 90, 180, Arc2D.OPEN);
-						g2d.setColor(Color.GRAY);
-						g2d.fill(punta);
-						g2d.setColor(Color.BLACK);
-						g2d.setStroke(new BasicStroke(2));
-						g2d.drawLine(x, y + TAMANIOCASILLA, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
-						break;
-					case BOTTOM:
-						punta = new Arc2D.Double(x, y - TAMANIOCASILLA, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 180, 180, Arc2D.OPEN);
-						g2d.setColor(Color.GRAY);
-						g2d.fill(punta);
-						g2d.setColor(Color.BLACK);
-						g2d.drawLine(x + TAMANIOCASILLA, y, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
-						break;
-					case RIGHT:
-						punta = new Arc2D.Double(x - TAMANIOCASILLA, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 270, 180, Arc2D.OPEN);
-						g2d.setColor(Color.GRAY);
-						g2d.fill(punta);
-						g2d.setColor(Color.BLACK);
-						g2d.drawLine(x, y + TAMANIOCASILLA, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
-						break;
-				}
-			} 
-		}
-		
-		if (punta != null) {
-			g2d.setColor(Color.BLACK);
-			g2d.draw(punta);
-		}
+			dc.dibujarCirculo(g2d, casilla, TAMANIOCASILLA, numCasillasAspa);
+//		Shape punta = null;
+//		g2d.setStroke(new BasicStroke(2));
+//		if (casilla.getLugarcirculo() != null) {
+//			int x = casilla.getCoordenadaX();
+//			int y = casilla.getCoordenadaY();
+//			if (numCasillasAspa % 2 == 0) {
+//				switch ( casilla.getLugarcirculo()) {
+//					case TOP:
+//						punta = new Arc2D.Double(x, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 0, 180, Arc2D.OPEN);
+//						g2d.setColor(Color.GRAY);
+//						g2d.fill(punta);
+//						g2d.setColor(Color.BLACK);
+//						g2d.drawLine(x + TAMANIOCASILLA, y, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
+//						break;
+//					case LEFT:
+//						punta = new Arc2D.Double(x, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 90, 180, Arc2D.OPEN);
+//						g2d.setColor(Color.GRAY);
+//						g2d.fill(punta);
+//						g2d.setColor(Color.BLACK);
+//						g2d.setStroke(new BasicStroke(2));
+//						g2d.drawLine(x, y + TAMANIOCASILLA, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
+//						break;
+//					case BOTTOM:
+//						punta = new Arc2D.Double(x, y - TAMANIOCASILLA, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 180, 180, Arc2D.OPEN);
+//						g2d.setColor(Color.GRAY);
+//						g2d.fill(punta);
+//						g2d.setColor(Color.BLACK);
+//						g2d.drawLine(x + TAMANIOCASILLA, y, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
+//						break;
+//					case RIGHT:
+//						punta = new Arc2D.Double(x - TAMANIOCASILLA, y, TAMANIOCASILLA * 2, TAMANIOCASILLA * 2, 270, 180, Arc2D.OPEN);
+//						g2d.setColor(Color.GRAY);
+//						g2d.fill(punta);
+//						g2d.setColor(Color.BLACK);
+//						g2d.drawLine(x, y + TAMANIOCASILLA, x + TAMANIOCASILLA, y + TAMANIOCASILLA);
+//						break;
+//				}
+//			} 
+//		}
+//		
+//		if (punta != null) {
+//			g2d.setColor(Color.BLACK);
+//			g2d.draw(punta);
+//		}
 	}
-		
+	
+	/**
+	 * Metodo que dibuja exclusivamente los triangulos del tablero, mediante los parametros recibidos
+	 * @param g2d
+	 * @param casilla 
+	 */
 	public void dibujarTriangulo(Graphics2D g2d,Casilla casilla) {
-		
-		int x = casilla.getCoordenadaX();
-		int y = casilla.getCoordenadaY();
-		Polygon triangulo = null;
-		Rectangle rect = new Rectangle();
-		g2d.setColor(Color.PINK);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.fill(rect);
-		g2d.setColor(Color.RED);
-
-		switch (casilla.getLugarTriangulo()) {
-			case TOP_LEFT:
-				triangulo = new Polygon(new int[]{x, x + TAMANIOCASILLA, x}, new int[]{y + (TAMANIOCASILLA/20), y + (TAMANIOCASILLA/2), y + (TAMANIOCASILLA / 2) * 2}, 3);
-				break;
-			case LEFT_TOP:
-				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/20), x+(TAMANIOCASILLA/2), x + (TAMANIOCASILLA/2) * 2}, new int[]{y, y + TAMANIOCASILLA, y}, 3);
-				break;
-			case LEFT_BOTTOM:
-				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/20),  x+(TAMANIOCASILLA/2), x + (TAMANIOCASILLA/2) * 2}, new int[]{y + TAMANIOCASILLA, y +2, y + TAMANIOCASILLA}, 3);
-				break;
-			case BOTTOM_LEFT:
-				triangulo = new Polygon(new int[]{x, x + TAMANIOCASILLA, x}, new int[]{y + (TAMANIOCASILLA/ 20), y +(TAMANIOCASILLA/2), y + (TAMANIOCASILLA/ 2) * 2}, 3);
-				break;
-			case BOTTOM_RIGHT:
-				triangulo = new Polygon(new int[]{x + TAMANIOCASILLA, x, x + TAMANIOCASILLA}, new int[]{y + (TAMANIOCASILLA / 20), y+(TAMANIOCASILLA/2), y + (TAMANIOCASILLA / 2) * 2}, 3);
-				break;
-			case RIGHT_BOTTOM:
-				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA / 20), x + (TAMANIOCASILLA/2), x + (TAMANIOCASILLA / 2) * 2}, new int[]{y + (TAMANIOCASILLA), y+2, y + (TAMANIOCASILLA)}, 3);
-				break;
-			case RIGHT_TOP:
-				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/ 20), x+ (TAMANIOCASILLA/2), x + (TAMANIOCASILLA/ 2)*2}, new int[]{y, y + TAMANIOCASILLA, y}, 3);
-				break;
-			case TOP_RIGHT:
-				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA), x, x + (TAMANIOCASILLA)}, new int[]{y + (TAMANIOCASILLA/20 ),  y + (TAMANIOCASILLA/2),  y + (TAMANIOCASILLA/ 2) * 2}, 3);
-				break;
-		}
-		
-		if (triangulo != null) {
-			g2d.fill(triangulo);
-			g2d.drawPolygon(triangulo);
-		}
-		g2d.setColor(Color.BLACK);
-		g2d.draw(rect);
+		dc.dibujarTriangulo(g2d, casilla, TAMANIOCASILLA);
+//		int x = casilla.getCoordenadaX();
+//		int y = casilla.getCoordenadaY();
+//		Polygon triangulo = null;
+//		Rectangle rect = new Rectangle();
+//		g2d.setColor(Color.PINK);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.fill(rect);
+//		g2d.setColor(Color.RED);
+//
+//		switch (casilla.getLugarTriangulo()) {
+//			case TOP_LEFT:
+//				triangulo = new Polygon(new int[]{x, x + TAMANIOCASILLA, x}, new int[]{y + (TAMANIOCASILLA/20), y + (TAMANIOCASILLA/2), y + (TAMANIOCASILLA / 2) * 2}, 3);
+//				break;
+//			case LEFT_TOP:
+//				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/20), x+(TAMANIOCASILLA/2), x + (TAMANIOCASILLA/2) * 2}, new int[]{y, y + TAMANIOCASILLA, y}, 3);
+//				break;
+//			case LEFT_BOTTOM:
+//				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/20),  x+(TAMANIOCASILLA/2), x + (TAMANIOCASILLA/2) * 2}, new int[]{y + TAMANIOCASILLA, y +2, y + TAMANIOCASILLA}, 3);
+//				break;
+//			case BOTTOM_LEFT:
+//				triangulo = new Polygon(new int[]{x, x + TAMANIOCASILLA, x}, new int[]{y + (TAMANIOCASILLA/ 20), y +(TAMANIOCASILLA/2), y + (TAMANIOCASILLA/ 2) * 2}, 3);
+//				break;
+//			case BOTTOM_RIGHT:
+//				triangulo = new Polygon(new int[]{x + TAMANIOCASILLA, x, x + TAMANIOCASILLA}, new int[]{y + (TAMANIOCASILLA / 20), y+(TAMANIOCASILLA/2), y + (TAMANIOCASILLA / 2) * 2}, 3);
+//				break;
+//			case RIGHT_BOTTOM:
+//				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA / 20), x + (TAMANIOCASILLA/2), x + (TAMANIOCASILLA / 2) * 2}, new int[]{y + (TAMANIOCASILLA), y+2, y + (TAMANIOCASILLA)}, 3);
+//				break;
+//			case RIGHT_TOP:
+//				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA/ 20), x+ (TAMANIOCASILLA/2), x + (TAMANIOCASILLA/ 2)*2}, new int[]{y, y + TAMANIOCASILLA, y}, 3);
+//				break;
+//			case TOP_RIGHT:
+//				triangulo = new Polygon(new int[]{x + (TAMANIOCASILLA), x, x + (TAMANIOCASILLA)}, new int[]{y + (TAMANIOCASILLA/20 ),  y + (TAMANIOCASILLA/2),  y + (TAMANIOCASILLA/ 2) * 2}, 3);
+//				break;
+//		}
+//		
+//		if (triangulo != null) {
+//			g2d.fill(triangulo);
+//			g2d.drawPolygon(triangulo);
+//		}
+//		g2d.setColor(Color.BLACK);
+//		g2d.draw(rect);
 	}
 	
+	/**
+	 * Metodo que dibuja la casilla exclusiva del jugador, mediante los parametros establecidos
+	 * @param g2d
+	 * @param casilla 
+	 */
 	public void dibujarPropia(Graphics2D g2d,Casilla casilla) {
-		Rectangle rect = new Rectangle();
-		g2d.setColor(Color.GREEN);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.fill(rect);
-		g2d.draw(rect);
-		g2d.setColor(Color.BLACK);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.draw(rect);
+		dc.dibujarPropia(g2d, casilla, TAMANIOCASILLA);
+//		Rectangle rect = new Rectangle();
+//		g2d.setColor(Color.GREEN);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.fill(rect);
+//		g2d.draw(rect);
+//		g2d.setColor(Color.BLACK);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.draw(rect);
 	}
 	
+	/**
+	 * Metodo que dibuja exclusivamente todas las casillas normales del tablero, mediante los parametros recibidos
+	 * @param g2d
+	 * @param casilla 
+	 */
 	public void dibujarNormal(Graphics2D g2d,Casilla casilla) {
-		Rectangle rect = new Rectangle();
-		g2d.setColor(Color.pink);
-		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
-		g2d.fill(rect);
-		g2d.setColor(Color.BLACK);
-		g2d.draw(rect);
+		dc.dibujarNormal(g2d, casilla, TAMANIOCASILLA);
+//		Rectangle rect = new Rectangle();
+//		g2d.setColor(Color.pink);
+//		rect.setBounds(casilla.getCoordenadaX(), casilla.getCoordenadaY(), TAMANIOCASILLA, TAMANIOCASILLA);
+//		g2d.fill(rect);
+//		g2d.setColor(Color.BLACK);
+//		g2d.draw(rect);
 	}
 }
