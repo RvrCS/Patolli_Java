d93e368423caecedf774471b4e1f937feb2dd1ca
RvrCS <75063620+RvrCS@users.noreply.github.com>
2022-11-09 20:27:15 -0700

    Backup Dominio

--- DominioPatolli/src/entidades/Jugador.java
+++ DominioPatolli/src/entidades/Jugador.java
@@ -1,40 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
-/**
- *
- * @author River
- */
 public class Jugador {
-    
-    private String nombre;
-    private int fondo;
 
-    public Jugador(String nombre, int fondo) {
-        this.nombre = nombre;
-        this.fondo = fondo;
-    }
+	private String nombre;
+	private int fondo;
+	private String color;
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Jugador(String nombre, int fondo) {
+		this.nombre = nombre;
+		this.fondo = fondo;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public Jugador() {
+	}
 
-    public int getFondo() {
-        return fondo;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setFondo(int fondo) {
-        this.fondo = fondo;
-    }
-    
-    
-    
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getFondo() {
+		return fondo;
+	}
+
+	public void setFondo(int fondo) {
+		this.fondo = fondo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
